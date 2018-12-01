c9106b49edb3fe63faa96b65560e2d7ac728a187
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-12-01 08:34:27 -0700

    Use IntSet instead of seq[Int]

--- puzzle-2/main.nim
+++ puzzle-2/main.nim
@@ -1,16 +1,17 @@
 import strutils
+import intsets
 
 const data = readFile("./puzzle-1/input.txt")
 
 var freq = 0
-var uniqueFreq : seq[int]
+var uniqueFreq : IntSet = initIntSet()
 
 var should_loop_continue = true
 
 while should_loop_continue:
 
     for line in data.splitLines():
-        uniqueFreq.add(freq)
+        uniqueFreq.incl(freq)
         if line.strip() == "":
             continue
         freq = freq + parseInt(line)
