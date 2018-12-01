a5a8b5851c82919a92fb2ed9d1fa29f80efc1d41
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-12-01 12:32:28 -0700

    Update day01.nim

--- 2017/nim/day01.nim
+++ 2017/nim/day01.nim
@@ -1,6 +1,6 @@
 import strutils
 
-proc solve(data: string): int =
+proc solve1(data: string): int =
 
     var s = 0
 
@@ -13,11 +13,38 @@ proc solve(data: string): int =
 
     return s
 
-assert solve("1122") == 3
-assert solve("1111") == 4
-assert solve("1234") == 0
-assert solve("91212129") == 9
+assert solve1("1122") == 3
+assert solve1("1111") == 4
+assert solve1("1234") == 0
+assert solve1("91212129") == 9
 
 const data = readFile("./inputs/day01.txt").strip()
 
-echo "Solution1: ", solve(data)
+echo "Solution1: ", solve1(data)
+
+proc find_c(data: string, i: int): char =
+    let l = data.len
+    let mid = int(l / 2)
+    if i < mid:
+        return data[i + mid]
+    else:
+        return data[( i + mid ) - l]
+
+proc solve2(data: string): int =
+
+    var s = 0
+    var cc = 'a'
+    for i, c in data:
+        cc = find_c(data, i)
+        if c == cc:
+            s = s + parseInt($c)
+
+    return s
+
+assert solve2("1212") == 6
+assert solve2("1221") == 0
+assert solve2("123425") == 4
+assert solve2("123123") == 12
+assert solve2("12131415") == 4
+
+echo "Solution1: ", solve2(data)
