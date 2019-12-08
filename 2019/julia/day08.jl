8b57410097ef6f92346ffce3e43cf296b50e28f6
Dheepak Krishnamurthy <me@kdheepak.com>
2019-12-07 23:29:52 -0700

    Update solution for day08

--- 2019/julia/day08.jl
+++ 2019/julia/day08.jl
@@ -4,24 +4,11 @@ data = read(joinpath(@__DIR__, "../data/day08.txt"), String)
 
 tdata = "123456789012"
 
-function layers(data, wide, tall)
-    data = strip(data)
-    l = [
-        [parse(Int, x) for x in data[i:i+(wide*tall)-1]] for i in 1:(wide*tall):length(data)
-    ]
-    image_layers = Vector{Vector{Int}}[]
-    for (i, il) in enumerate(l)
-        push!(image_layers, [
-            [x for x in (il[j:j+wide-1])] for j in 1:wide:length(il)
-        ])
-    end
-    return image_layers
-end
+layers(data::String, wide, tall) = layers(Int[parse(Int, i) for i in strip(data)], wide, tall)
+layers(data::Vector{Int}, wide, tall) = reshape(data, wide, tall, length(data) ÷ (wide * tall))
 
-@assert layers(tdata, 3, 2) == [
-    [[1, 2, 3], [4, 5, 6]],
-    [[7, 8, 9], [0, 1, 2]]
-]
+@assert layers(tdata, 3, 2)[:, :, 1] == [1 2 3; 4 5 6]'
+@assert layers(tdata, 3, 2)[:, :, 2] == [7 8 9; 0 1 2]'
 
 wide = 25
 tall = 6
@@ -30,39 +17,35 @@ LAYERS = layers(data, wide, tall)
 function corruption_check(layers)
     minz = Inf
     minl = nothing
-    for l in layers
-        z = sum(count(x -> x==0, il) for il in l)
+    for il in 1:size(layers)[3]
+        l = layers[:, :, il]'
+        z = count(x -> x==0, l)
         if min(minz, z) < minz
             minz = min(minz, z)
             minl = l
         end
     end
-    ones = sum(count(x -> x==1, l) for l in minl)
-    twos = sum(count(x -> x==2, l) for l in minl)
+    ones = count(x -> x==1, minl)
+    twos = count(x -> x==2, minl)
     return ones * twos
 end
 
 @assert corruption_check(LAYERS) == 828
 
-function merge(layer1::Vector{Vector{Int}}, layer2::Vector{Vector{Int}})
-    new_layer = Vector{Int}[]
-    for (line1, line2) in zip(layer1, layer2)
-        push!(new_layer, [
-            merge(b1, b2) for (b1, b2) in zip(line1, line2)
-        ])
-    end
-    return new_layer
+function merge(layer1::Matrix{Int}, layer2::Matrix{Int})
+    return reshape([merge(p1, p2) for (p1, p2) in zip(layer1, layer2)], size(layer1)...)
 end
 
 merge(pixel1::Int, pixel2::Int) = (pixel1 == 0 || pixel1 == 1) ? pixel1 : (pixel2 == 0 || pixel2 == 1) ? pixel2 : 2
 
-for x in foldl(merge, LAYERS)
-    for c in x
-        if c == 0
-            print(Crayon(), " ", Crayon(reset = true))
-        elseif c == 1
-            print(Crayon(background = :black), " ", Crayon(reset = true))
-        end
+result = foldl(merge, [LAYERS[:, :, z] for z in 1:size(LAYERS)[3]])'
+rows, cols = size(result)
+for row in 1:rows
+    for col in 1:cols
+        c = result[row, col]
+        c == 0 ? print(Crayon(), " ", Crayon(reset = true)) :
+        c == 1 ? print(Crayon(background = :black), " ", Crayon(reset = true)) :
+        error("c != 1 || c != 0 : c = $c")
     end
     println()
 end
