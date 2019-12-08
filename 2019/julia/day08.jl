86415aa1467920d854d303f746938740bff34778
Dheepak Krishnamurthy <me@kdheepak.com>
2019-12-07 23:44:50 -0700

    Update solution for day08

--- 2019/julia/day08.jl
+++ 2019/julia/day08.jl
@@ -30,20 +30,21 @@ end
 
 @assert corruption_check(LAYERS) == 828
 
-function merge(layer1::Matrix{Int}, layer2::Matrix{Int})
-    return reshape([merge(p1, p2) for (p1, p2) in zip(layer1, layer2)], size(layer1)...)
-end
-
+merge(layer1::Matrix{Int}, layer2::Matrix{Int}) = reshape([merge(p1, p2) for (p1, p2) in zip(layer1, layer2)], size(layer1)...)
 merge(pixel1::Int, pixel2::Int) = (pixel1 == 0 || pixel1 == 1) ? pixel1 : (pixel2 == 0 || pixel2 == 1) ? pixel2 : 2
 
-result = foldl(merge, [LAYERS[:, :, z] for z in 1:size(LAYERS)[3]])'
-rows, cols = size(result)
-for row in 1:rows
-    for col in 1:cols
-        c = result[row, col]
-        c == 0 ? print(Crayon(), " ", Crayon(reset = true)) :
-        c == 1 ? print(Crayon(background = :black), " ", Crayon(reset = true)) :
-        error("c != 1 || c != 0 : c = $c")
+function draw(image)
+    result = foldl(merge, [image[:, :, z] for z in 1:size(image)[3]])'
+    rows, cols = size(result)
+    for row in 1:rows
+        for col in 1:cols
+            c = result[row, col]
+            c == 0 ? print(Crayon(), " ", Crayon(reset = true)) :
+            c == 1 ? print(Crayon(background = :black), " ", Crayon(reset = true)) :
+            error("c != 1 || c != 0 : c = $c")
+        end
+        println()
     end
-    println()
 end
+
+draw(LAYERS)
