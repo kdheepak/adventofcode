ff176102d8e0dad37e026bac6d624622eda7dada
Dheepak Krishnamurthy <me@kdheepak.com>
2019-12-11 12:02:33 -0700

    Update files

--- 2019/julia/day11.jl
+++ 2019/julia/day11.jl
@@ -5,70 +5,36 @@ data = read(joinpath(@__DIR__, "../data/day11.txt"), String)
 include("vm.jl")
 
 function part1(start_panel_color = 0)
-
     inp = Channel{Int}(1)
     out = Channel{Int}(1)
     vm = VM(data, input = inp, output = out)
-
     @async run!(vm)
-
-    current = [0, 0]
-    panels = DefaultDict{Tuple{Int, Int}, Int}(0)
-    painted = DefaultDict{Tuple{Int, Int}, Int}(0)
-    movement = Dict{Symbol, Vector{Int}}(:up => [0, 1], :left => [-1, 0], :down => [0, -1], :right => [1, 0])
-    facing = :up
-    panels[current...] = start_panel_color
-    while vm.halted == false
-
-        put!(inp, panels[current...])
-
-        panels[current...] = take!(out)
-        painted[current...] += 1
-
+    current = 0 + 0im
+    panels = DefaultDict(0, [(current, 0)])
+    facing = 0 + 1im
+    panels[current] = start_panel_color
+    while !vm.halted
+        put!(inp, panels[current])
+        panels[current] = take!(out)
         direction = take!(out)
-        if direction == 0 && facing == :up
-            facing = :left
-        elseif direction == 1 && facing == :up
-            facing = :right
-        elseif direction == 0 && facing == :left
-            facing = :down
-        elseif direction == 1 && facing == :left
-            facing = :up
-        elseif direction == 0 && facing == :down
-            facing = :right
-        elseif direction == 1 && facing == :down
-            facing = :left
-        elseif direction == 0 && facing == :right
-            facing = :up
-        elseif direction == 1 && facing == :right
-            facing = :down
-        else
-            error("Unknown facing: $facing")
-        end
-
-        x, y = movement[facing]
-        current[1] += x
-        current[2] += y
-
+        turn = direction == 0 ? -im : im
+        facing *= turn
+        current += facing
     end
-
-    return painted, panels
-
+    return panels
 end
 
-painted, panels = part1()
-@assert length(painted) == 1771
+panels = part1()
+@assert length(panels) == 1771
 
 function part2()
-    _, panels = part1(1)
-    maxx = maximum(x for (x, y) in keys(panels))
-    minx = minimum(x for (x, y) in keys(panels))
-    miny = minimum(y for (x, y) in keys(panels))
-    maxy = maximum(y for (x, y) in keys(panels))
+    panels = part1(1)
+    minx, maxx = extrema(real.(keys(panels)))
+    miny, maxy = extrema(imag.(keys(panels)))
     io = IOBuffer()
     for y in maxy:-1:miny
-        for x in minx:maxx
-            print(io, panels[x, y] == 1 ? "█" : " ")
+        for x in maxx:-1:minx
+            print(io, panels[x + y*im] == 1 ? "█" : " ")
         end
         println(io)
     end
