2460e6d20c94309ca2f4d8729a51b55c6fda28c6
Dheepak Krishnamurthy <me@kdheepak.com>
2019-12-13 14:05:24 -0700

    Add playable version

--- 2019/julia/aoc/src/day13/day13.jl
+++ 2019/julia/aoc/src/day13/day13.jl
@@ -1,6 +1,10 @@
 readInput() = read(joinpath(@__DIR__, "./input.txt"), String)
 
 using DataStructures
+using TERMIOS
+using REPL: Terminals
+
+const T = TERMIOS
 
 include("../vm.jl")
 
@@ -9,28 +13,6 @@ function part1(data = readInput())
     run!(vm)
     outputs = output(vm)
     return length([i for i in 3:3:length(outputs) if outputs[i] == 2])
-end
-
-function draw(game)
-    if length(keys(game)) == 0
-        return
-    end
-    minx, maxx = extrema(x for (x,y) in keys(game))
-    miny, maxy = extrema(y for (x,y) in keys(game))
-    for y in miny:maxy
-        for x in minx:maxx
-            if game[(x,y)] == 4
-                print("O")
-            elseif game[(x,y)] == 3
-                print("█")
-            elseif game[(x,y)] == 0
-                print(" ")
-            else
-                print("█")
-            end
-        end
-        println()
-    end
 end
 
 function part2(data = readInput())
@@ -75,6 +57,99 @@ function part2(data = readInput())
         end
         game[(x, y)] = tile
     end
+    return score
+end
+
+function draw(game, score)
+    print("$(Terminals.CSI)H")
+    print("$(Terminals.CSI)?25l")
+    if length(keys(game)) == 0
+        return
+    end
+    minx, maxx = extrema(x for (x,y) in keys(game))
+    miny, maxy = extrema(y for (x,y) in keys(game))
+    for y in miny:maxy
+        for x in minx:maxx
+            if game[(x,y)] == 4
+                print("O")
+            elseif game[(x,y)] == 3
+                print("█")
+            elseif game[(x,y)] == 0
+                print(" ")
+            else
+                print("█")
+            end
+        end
+        println()
+    end
+    print("$(Terminals.CSI)?12l$(Terminals.CSI)?25l")
+end
+
+function read_character()
+    String(read(stdin, 1))
+end
+
+function play(data = readInput())
+    print("$(Terminals.CSI)0;1H")
+    print("$(Terminals.CSI)2J")
+    new_term = T.termios()
+    old_term = T.termios()
+    T.tcgetattr(stdin, old_term)
+    T.tcgetattr(stdin, new_term)
+    data = [parse(Int, x) for x in split(strip(data), ",")]
+    data[1] = 2
+    vm = VM(data)
+    @async run!(vm)
+    game = DefaultDict{Tuple{Int, Int}, Int}(0)
+    score = 0
+    ball = -1, -1
+    paddle = -1, -1
+    new_term.c_lflag &= ~(T.ICANON | T.ECHO)
+    cc = [c for c in new_term.c_cc]
+    new_term.c_cc = tuple(cc...)
+    T.tcsetattr(stdin, T.TCSANOW, new_term)
+    @async while true
+        c = String(read(stdin, 1))
+        if c == "l"
+            put!(vm.input, 1)
+        elseif c == "h"
+            put!(vm.input, -1)
+        elseif c == "q"
+            break
+        else
+            put!(vm.input, 0)
+        end
+        yield()
+    end
+    while !vm.halted
+        wait(vm.output)
+        while Base.n_avail(vm.output) > 0
+            x = take!(vm.output)
+            y = take!(vm.output)
+            tile = take!(vm.output)
+            if x == -1 && y == 0
+                score = tile
+            elseif tile == 4
+                ball = (x, y)
+            elseif tile == 3
+                paddle = (x, y)
+            else
+            end
+            game[(x, y)] = tile
+        end
+        draw(game, score)
+    end
+    while Base.n_avail(vm.output) > 0
+        x = take!(vm.output)
+        y = take!(vm.output)
+        tile = take!(vm.output)
+        if x == -1 && y == 0
+            score = tile
+        end
+        game[(x, y)] = tile
+    end
+    T.tcsetattr(stdin, T.TCSANOW, old_term)
+    print("$(Terminals.CSI)?12l$(Terminals.CSI)?25l")
     return score
 end
 
