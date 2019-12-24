fc01345cfb01ff096cc1620ba3b25ed26273ab78
Dheepak Krishnamurthy <me@kdheepak.com>
2019-12-23 22:54:34 -0700

    Update files

--- 2019/julia/aoc/src/day24/day24.jl
+++ 2019/julia/aoc/src/day24/day24.jl
@@ -3,3 +3,76 @@ readInput() = read(joinpath(@__DIR__, "./input.txt"), String)
 part1(data = readInput()) = nothing
 
 part2(data = readInput()) = nothing
+
+function parse_area(data = readInput())
+    data = strip(data)
+    area = Array(reshape([c == '#' ? 1 : 0 for c in data if c != '\n'], length(split(data, "\n")), :)')
+end
+
+function simulate_life!(area)
+    X, Y = size(area)
+    status = Dict{Tuple{Int, Int}, Int}()
+    for y in 1:Y
+        for x in 1:X
+            s = 0
+            try
+                s += area[x - 1, y]
+            catch end
+            try
+                s += area[x, y - 1]
+            catch end
+            try
+                s += area[x, y + 1]
+            catch end
+            try
+                s += area[x + 1, y]
+            catch end
+            if area[x, y] == 1 && s != 1
+                status[(x, y)] = -1
+            elseif area[x, y] == 0 && (s == 1 || s == 2)
+                status[(x, y)] = 1
+            else
+                status[(x, y)] = 0
+            end
+        end
+    end
+    for (x,y) in keys(status)
+        area[x, y] += status[(x, y)]
+    end
+end
+
+function find_cycle(data = readInput())
+
+    area = parse_area(data)
+
+    hashes = Dict{UInt64, Matrix{Int}}()
+    h = hash(area)
+    while h ∉ keys(hashes)
+        hashes[h] = copy(area)
+        simulate_life!(area)
+        h = hash(area)
+    end
+
+    return hashes[h]
+end
+
+function calculate_biodiversity_rating(area)
+    X, Y = size(area)
+    exponent = 1
+    s = 0
+    for x in 1:X
+        for y in 1:Y
+            s += (2 ^ exponent) ÷ 2 * area[x, y]
+            exponent += 1
+        end
+    end
+    return s
+end
+
+test_data = """
+....#
+#..#.
+#..##
+..#..
+#....
+"""
