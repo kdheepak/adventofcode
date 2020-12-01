502d211b78204e1bf6c845368d2a4864a410c5e7
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-01 01:05:45 -0700

    Update 2020

--- 2020/julia/aoc/src/day01/script.jl
+++ 2020/julia/aoc/src/day01/script.jl
@@ -1,28 +1,24 @@
+using DataStructures
+using Combinatorics
 
 readInput() = read(joinpath(@__DIR__, "./input.txt"), String)
 
-function expense_report(data)
-    for (i,item1) in enumerate(data)
-        for (j,item2) in enumerate(data)
-            for (k,item3) in enumerate(data)
-                if item1 + item2 + item3 == 2020 && i != j != k
-                    return item1, item2, item3
-                end
-            end
+function expense_report(data, n)
+    for items in combinations(data, n)
+        if sum(items) == 2020
+            return prod(items)
         end
     end
-end
+end 
 
 function part1(data = readInput()) 
-    i1, i2 = expense_report(parse.(Int, split(strip(data))))
-    i1 * i2
+    expense_report(parse.(Int, split(strip(data))), 2)
 end
 
 @show part1()
 
 function part2(data = readInput()) 
-    i1, i2, i3 = expense_report(parse.(Int, split(strip(data))))
-    i1 * i2 * i3
+    expense_report(parse.(Int, split(strip(data))), 3)
 end
 
 @show part2()
