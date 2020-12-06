3d83b2babef25243276e9f0f17f5871c53e458da
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-05 22:53:15 -0700

    Update files

--- 2020/julia/src/day06/script.jl
+++ 2020/julia/src/day06/script.jl
@@ -3,13 +3,13 @@ readInput() = strip(read(joinpath(@__DIR__, "./input.txt"), String))
 function part1(data = readInput())
     mapreduce(+, split(data, "\n\n")) do group
         questions = split(group, '\n', keepempty=false)
-        length(union(Set.(questions)...))
+        length(∪(Set.(questions)...))
     end
 end
 
 function part2(data = readInput())
     mapreduce(+, split(data, "\n\n")) do group
         questions = split(group, '\n', keepempty=false)
-        length(intersect(Set.(questions)...))
+        length(∩(Set.(questions)...))
     end
 end
