ecf3e752e9a2f195553dc4b43203a6716d835936
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-05 22:11:49 -0700

    Update files

--- 2020/julia/src/day06/script.jl
+++ 2020/julia/src/day06/script.jl
@@ -1,5 +1,39 @@
-readInput() = read(joinpath(@__DIR__, "./input.txt"), String)
+readInput() = strip(read(joinpath(@__DIR__, "./input.txt"), String))
 
-part1(data = readInput()) = nothing
+function part1(data = readInput())
+    data = split(data, "\n\n")
+    total = 0
+    for group in data
+        questions = split(group, '\n')
+        counter = Set()
+        for question in questions
+            for c in question
+                push!(counter, c)
+            end
+        end
+        total += length(counter)
+    end
+    total
+end
 
-part2(data = readInput()) = nothing
+function part2(data = readInput())
+    data = split(data, "\n\n")
+    total = 0
+    for group in data
+        questions = split(group, '\n', keepempty=false)
+        counter_set = Set()
+        for (i, question) in enumerate(questions)
+            counter = Set()
+            for c in question
+                push!(counter, c)
+            end
+            if i == 1
+                counter_set = counter
+            else
+                counter_set = intersect(counter_set, counter)
+            end
+        end
+        total += length(counter_set)
+    end
+    total
+end
