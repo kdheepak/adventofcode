7bc32d7773b87e084535f03663e22ad8844da469
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-05 22:33:29 -0700

    Update files

--- 2020/julia/src/day06/script.jl
+++ 2020/julia/src/day06/script.jl
@@ -1,39 +1,15 @@
 readInput() = strip(read(joinpath(@__DIR__, "./input.txt"), String))
 
 function part1(data = readInput())
-    data = split(data, "\n\n")
-    total = 0
-    for group in data
+    mapreduce(+, split(data, "\n\n")) do group
         questions = split(group, '\n', keepempty=false)
-        counter = Set()
-        for question in questions
-            for c in question
-                push!(counter, c)
-            end
-        end
-        total += length(counter)
+        length(union(Set.(questions)...))
     end
-    total
 end
 
 function part2(data = readInput())
-    data = split(data, "\n\n")
-    total = 0
-    for group in data
+    mapreduce(+, split(data, "\n\n")) do group
         questions = split(group, '\n', keepempty=false)
-        counter_set = Set()
-        for (i, question) in enumerate(questions)
-            counter = Set()
-            for c in question
-                push!(counter, c)
-            end
-            if i == 1
-                counter_set = counter
-            else
-                counter_set = intersect(counter_set, counter)
-            end
-        end
-        total += length(counter_set)
+        length(intersect(Set.(questions)...))
     end
-    total
 end
