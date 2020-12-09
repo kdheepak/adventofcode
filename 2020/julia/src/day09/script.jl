52d8c551f9d46eaa6d58ebe1a0057f4e7e87a705
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-08 22:50:22 -0700

    Update files

--- 2020/julia/src/day09/script.jl
+++ 2020/julia/src/day09/script.jl
@@ -1,8 +1,8 @@
 readInput() = strip(read(joinpath(@__DIR__, "./input.txt"), String))
 
-part1(data = readInput()) = f(parse.(Int,split(data, '\n')))
+part1(data = readInput()) = solution1(parse.(Int,split(data, '\n')))
 
-function f(numbers)
+function solution1(numbers)
     preamble = 25
     for i in (preamble + 1):length(numbers)
         check(numbers[i-preamble:i-1], numbers[i]) && continue
@@ -20,7 +20,7 @@ end
 part2(data = readInput()) = solution2(parse.(Int,split(data, '\n')))
 
 function solution2(numbers)
-    idx, num = f(numbers)
+    idx, num = solution1(numbers)
     for i in eachindex(numbers), j in i:lastindex(numbers)
         sum(numbers[i:j]) == num && return sum(extrema(numbers[i:j]))
     end
