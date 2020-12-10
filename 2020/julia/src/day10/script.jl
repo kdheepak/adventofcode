623e2faa293cbd8c8d00a0b72be205b55c7f3b0c
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-10 01:12:07 -0700

    Update files

--- 2020/julia/src/day10/script.jl
+++ 2020/julia/src/day10/script.jl
@@ -35,5 +35,35 @@ function j(data)
 end
 
 function c(data)
-    data
+    _, data = j(data)
+    push!(data, data[end] + 3)
+    pushfirst!(data, 0)
+    memoize = Dict{Int, Int}()
+    check(data, 1, memoize)
+end
+
+function check(data, i, memoize)
+    @show data, data[i], memoize
+    if i ∈ keys(memoize)
+        println("Already found path $i: $(data[i]) -> $(data[end]): $(memoize)")
+        return memoize[i]
+    end
+    if i == length(data)
+        println("Found a valid path: $memoize")
+        return 1
+    end
+    counter = 0
+    if i + 3 <= length(data) && data[i + 3] - data[i] <= 3
+        @show data[i:i+3]
+        counter += check(data, i + 3, memoize)
+    end
+    if i + 2 <= length(data) && data[i + 2] - data[i] <= 3
+        @show data[i:i+2]
+        counter += check(data, i + 2, memoize)
+    end
+    if i + 1 <= length(data) && data[i + 1] - data[i] <= 3
+        @show data[i:i+1]
+        counter += check(data, i + 1, memoize)
+    end
+    return memoize[i] = counter
 end
