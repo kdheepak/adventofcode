47d4baa40405cf1949accfb8188a825b88627db3
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-17 00:38:21 -0700

    Update files

--- 2020/julia/src/day17/script.jl
+++ 2020/julia/src/day17/script.jl
@@ -23,7 +23,7 @@ function f(data)
     pocket = Dict{NTuple{3, Int64}, Bool}()
 
     for j in 1:size(grid, 2), i in 1:size(grid, 1)
-        pocket[(i, j, 0)] = grid[i, j]
+        pocket[(i, j, 1)] = grid[i, j]
     end
 
     cycle = 1
@@ -75,6 +75,12 @@ for i in (0, 1, -1), j in (0, 1, -1), k in (0, 1, -1), l in (0, 1, -1)
     push!(NEIGHBORS2, CartesianIndex(i, j, k, l))
 end
 
+const NEIGHBORS3 = CartesianIndex[]
+
+for i in (0, 1, -1, 2, -2), j in (0, 1, -1, 2, -2), k in (0, 1, -1, 2, -2), l in (0, 1, -1, 2, -2)
+    push!(NEIGHBORS3, CartesianIndex(i, j, k, l))
+end
+
 function g(data)
     data = permutedims(reduce(hcat, collect.(split(data, '\n'))))
     grid = zeros(Bool, size(data)...)
@@ -86,18 +92,18 @@ function g(data)
     pocket = Dict{NTuple{4, Int64}, Bool}()
 
     for j in 1:size(grid, 2), i in 1:size(grid, 1)
-        pocket[(i, j, 0, 0)] = grid[i, j]
+        pocket[(i, j, 1, 1)] = grid[i, j]
     end
 
     cycle = 1
     while true
-        cycle == 2 && break
+        cycle > 1 && break
         seen = Set{NTuple{4, Int64}}()
         tmp = Dict{NTuple{4, Int64}, Bool}()
-        for k in collect(keys(pocket)), n in NEIGHBORS2
-            xk, yk, zk, ak = k
-            xn, yn, zn, an = n.I
-            push!(seen, (xk + xn, yk + yn, zk + zn, ak + an))
+        for k in collect(keys(pocket)), n in NEIGHBORS3
+            xk, yk, zk, wk = k
+            xn, yn, zn, wn = n.I
+            push!(seen, (xk + xn, yk + yn, zk + zn, wk + wn))
         end
         for K in seen
             tmp[K] = find_next_state2(pocket, K...)
@@ -105,27 +111,39 @@ function g(data)
         for k in keys(tmp)
             pocket[k] = tmp[k]
         end
+        visualize(pocket)
         cycle += 1
     end
-    pocket
 end
 
-function find_next_state2(pocket, x, y, z, a)
-    counter = []
-    for index in NEIGHBORS2
-        xindex, yindex, zindex, aindex = index.I
-        xindex == 0 && yindex == 0 && zindex == 0 && aindex == 0 && continue
-        if haskey(pocket, (x + xindex, y + yindex, z + zindex, a + aindex))
-            push!(counter, pocket[(x + xindex, y + yindex, z + zindex, a + aindex)] == 1)
-        else
-            push!(counter, false)
+function visualize(pocket)
+    w = 0
+    for w in (-1, 0, 1), z in (-1, 0, 1)
+        @show z, w
+        for x in 1:3
+            for y in 1:3
+                print(haskey(pocket, (x, y, z, w)) && pocket[(x,y,z,w)] ? "#" : ".")
+            end
+            println()
         end
+    end
+end
+
+function find_next_state2(pocket, x, y, z, w)
+    counter = []
+    for (i, index) in enumerate(NEIGHBORS2)
+        xindex, yindex, zindex, windex = index.I
+        xindex == 0 && yindex == 0 && zindex == 0 && windex == 0 && continue
+        c = haskey(pocket, (x + xindex, y + yindex, z + zindex, w + windex)) ? pocket[(x + xindex, y + yindex, z + zindex, w + windex)] == 1 : false
+        cell = c ? '#' : '.'
+        x == 1 && y == 1 && z == 0 && w == 0 && @show x + xindex, y + yindex, z + zindex, w + windex, cell, i
+        push!(counter, c)
     end
     if ( count(counter) == 2 || count(counter) == 3 ) && haskey(pocket, (x, y, z)) && pocket[x, y, z]
         return true
-    elseif !haskey(pocket, (x, y, z, a)) && count(counter) == 3
+    elseif !haskey(pocket, (x, y, z, w)) && count(counter) == 3
         return true
-    elseif haskey(pocket, (x, y, z, a)) && pocket[x, y, z, a] == false && count(counter) == 3
+    elseif haskey(pocket, (x, y, z, w)) && pocket[x, y, z, w] == false && count(counter) == 3
         return true
     else
         return false
