b5b421d0fd096fc562a0ba651b1331852f152e78
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-17 23:19:25 -0700

    Update files

--- 2020/julia/src/day18/script.jl
+++ 2020/julia/src/day18/script.jl
@@ -1,5 +1,38 @@
-readInput() = read(joinpath(@__DIR__, "./input.txt"), String)
+readInput() = strip(read(joinpath(@__DIR__, "./input.txt"), String))
 
-part1(data = readInput()) = nothing
+part1(data = readInput()) = f(String.(split(data, '\n')))
 
-part2(data = readInput()) = nothing
+part2(data = readInput()) = g(String.(split(data, '\n')))
+
+import Base.:+
+import Base.:*
+
+struct Custom
+    i::Int
+end
+
+*(x::Custom, y::Custom) = Custom(x.i * y.i)
++(x::Custom, y::Custom) = Custom(x.i + y.i)
+
+-(x::Custom, y::Custom) = Custom(x.i * y.i)
+/(x::Custom, y::Custom) = Custom(x.i + y.i)
+
+function f(data)
+    data = map(data) do line
+        line = replace(line, r"(\d)" => s"Custom(\1)")
+        line = replace(line, "*" => "-")
+        Meta.parse(line)
+    end
+    data = eval.(data)
+    sum(d.i for d in data)
+end
+
+function g(data)
+    data = map(data) do line
+        line = replace(line, r"(\d)" => s"Custom(\1)")
+        line = replace(replace(line, "*" => "-"), "+" => "/")
+        Meta.parse(line)
+    end
+    data = eval.(data)
+    sum(d.i for d in data)
+end
