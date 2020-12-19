e451739afb846f1533f59744f84ff166fc93df4a
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-19 14:38:25 -0700

    Update files

--- 2020/julia/src/day19/script.jl
+++ 2020/julia/src/day19/script.jl
@@ -2,60 +2,48 @@ readInput() = strip(read(joinpath(@__DIR__, "./input.txt"), String))
 
 part1(data = readInput()) = f(split(strip(data), "\n\n")...)
 
-part2(data = readInput()) = nothing
-
+part2(data = readInput()) = g(split(strip(data), "\n\n")...)
 
 function f(rules, messages)
     rules = split(rules, '\n')
     rules = split.(rules, ": ")
-    rules = map(rules) do (n, rule)
-        subrules = String.(split(rule, '|'))
-        if length(subrules) == 1 && startswith(subrules[1], '"') && endswith(subrules[1], '"')
-            subrules[1] = String(replace(subrules[1], '"' => ""))
-        else
-            subrules = map(subrules) do subrule
-                subrule = parse.(Int, split(subrule, " ", keepempty = false))
-                subrule
-            end
-        end
-        parse(Int, n), subrules
+    rules = map(rules) do (n, subrules)
+        subrules = strip(subrules, '\"')
+        n => subrules
     end
-
-    rules = sort(rules, by = x -> x[1])
-
+    rules = Dict(rules...)
     messages = String.(split(messages, '\n'))
-
-    memo = Dict()
-    count(map(messages) do m
-        r, i = matcher!(m, 1, rules[1], rules, memo)
-        @show m[i:end]
-        r
-    end)
+    r = replace(regex_builder(rules, "0"), " " => "")
+    count(!=(nothing), match.(Regex("^$r\$"), messages))
 end
 
+function regex_builder(rules, n)
+    subrules = rules[n]
+    if length(subrules) == 1 && ( subrules[1] == "a" || subrules[1] == "b" )
+        return subrules[1]
+    end
+    for r in eachmatch(r"(\d+)", subrules)
+        subrules = replace(subrules, r[1] => regex_builder(rules, r[1]), count = 1)
+    end
+    if occursin("|", subrules)
+        return "( " * subrules * " )"
+    else
+        return subrules
+    end
+end
 
-function matcher!(s, index, rule, rules, memo)
-    if (s, index, rule) ∈ keys(memo)
-        return memo[(s, index, rule)]
+function g(rules, messages)
+    rules = split(rules, '\n')
+    rules = split.(rules, ": ")
+    rules = map(rules) do (n, subrules)
+        subrules = strip(subrules, '\"')
+        n => subrules
     end
-    n, subrules = rule
-    if length(subrules) == 1 && subrules[1] isa String
-        c = first(s[index:end])
-        memo[(s, index, rule)] = (string(c) == subrules[1], index)
-        return string(c) == subrules[1], index
-    end
-    m = Bool[]
-    for subrule in subrules
-        matches = Bool[]
-        tindex = index
-        for (i, r) in enumerate(subrule)
-            result, tindex = matcher!(s, tindex + i - 1, rules[r + 1], rules, memo)
-            push!(matches, result)
-        end
-        @show matches
-        push!(m, all(matches))
-    end
-    @show index, s[index:end], m
-    memo[(s, index, rule)] = (any(m), index + length(subrules[1]))
-    return any(m), index + length(subrules[1])
+
+    rules = Dict(rules...)
+    messages = String.(split(messages, '\n'))
+    rules["8"] = "(42)+"
+    rules["11"] = "42 31 | 42 ( 42 31 | 42 ( 42 31 | 42 ( 42 31 | 42 ( 42 31 ) 31 ) 31 ) 31 ) 31"
+    r = replace(regex_builder(rules, "0"), " " => "")
+    count(!=(nothing), match.(Regex("^$r\$"), messages))
 end
