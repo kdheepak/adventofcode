f7c366d94affffd0869bcc87dc33fc48506ad513
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-18 23:48:26 -0700

    Update files

--- 2020/julia/src/day19/script.jl
+++ 2020/julia/src/day19/script.jl
@@ -21,22 +21,27 @@ function f(rules, messages)
         parse(Int, n), subrules
     end
 
+    rules = sort(rules, by = x -> x[1])
+
     messages = String.(split(messages, '\n'))
 
+    memo = Dict()
     count(map(messages) do m
-        r, i = matcher!(m, 1, rules[1], rules)
+        r, i = matcher!(m, 1, rules[1], rules, memo)
+        @show m[i:end]
         r
     end)
 end
 
 
-function matcher!(s, index, rule, rules)
-    if s == ""
-        return false, index
+function matcher!(s, index, rule, rules, memo)
+    if (s, index, rule) ∈ keys(memo)
+        return memo[(s, index, rule)]
     end
     n, subrules = rule
     if length(subrules) == 1 && subrules[1] isa String
         c = first(s[index:end])
+        memo[(s, index, rule)] = (string(c) == subrules[1], index)
         return string(c) == subrules[1], index
     end
     m = Bool[]
@@ -44,10 +49,13 @@ function matcher!(s, index, rule, rules)
         matches = Bool[]
         tindex = index
         for (i, r) in enumerate(subrule)
-            result, tindex = matcher!(s, tindex + i - 1, rules[r + 1], rules)
+            result, tindex = matcher!(s, tindex + i - 1, rules[r + 1], rules, memo)
             push!(matches, result)
         end
+        @show matches
         push!(m, all(matches))
     end
+    @show index, s[index:end], m
+    memo[(s, index, rule)] = (any(m), index + length(subrules[1]))
     return any(m), index + length(subrules[1])
 end
