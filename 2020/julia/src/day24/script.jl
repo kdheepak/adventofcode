c4f622ce6c119eb1e546efdd33014b3e53124e5d
Dheepak Krishnamurthy <me@kdheepak.com>
2020-12-24 00:29:37 -0700

    Update files

--- 2020/julia/src/day24/script.jl
+++ 2020/julia/src/day24/script.jl
@@ -1,5 +1,3 @@
-using DataStructures
-
 readInput() = strip(read(joinpath(@__DIR__, "./input.txt"), String))
 
 part1(data = readInput()) = f(strip(data))
@@ -17,7 +15,7 @@ function f(data)
     tiles = split(data, '\n')
     tiles = find_steps.(tiles)
 
-    grid = DefaultDict{Complex, Bool}(false)
+    grid = Dict{Complex, Bool}()
     for tile in tiles
         flip!(grid, tile)
     end
@@ -56,35 +54,86 @@ function flip!(grid, tiles)
     reference = 1 + 1im
     for n in tiles
         if n == East()
-            reference += round(exp(im * 0), digits = 4)
+            reference += round(exp(im * 0), digits = 2)
         elseif n == NorthEast()
-            reference += round(exp(im * π / 3), digits = 4)
+            reference += round(exp(im * π / 3), digits = 2)
         elseif n == NorthWest()
-            reference += round(exp(im * 2π / 3), digits = 4)
+            reference += round(exp(im * 2π / 3), digits = 2)
         elseif n == West()
-            reference += round(exp(im * π), digits = 4)
+            reference += round(exp(im * π), digits = 2)
         elseif n == SouthWest()
-            reference += round(exp(im * ( π + π / 3 )), digits = 4)
+            reference += round(exp(im * ( π + π / 3 )), digits = 2)
         elseif n == SouthEast()
-            reference += round(exp(im * ( π + 2π / 3 )), digits = 4)
+            reference += round(exp(im * ( π + 2π / 3 )), digits = 2)
         else
             error("wat")
         end
     end
-    reference = round(reference, digits = 4)
-    # @show reference, grid[reference], !grid[reference]
+    reference = round(reference, digits = 2)
+    reference ∉ keys(grid) && ( grid[reference] = false )
     grid[reference] = !grid[reference]
+end
+
+function has_neighbour(grid, k)
+    directions = [
+        round(exp(im * 0), digits = 2),
+        round(exp(im * π / 3), digits = 2),
+        round(exp(im * 2π / 3), digits = 2),
+        round(exp(im * π), digits = 2),
+        round(exp(im * ( π + π / 3 )), digits = 2),
+        round(exp(im * ( π + 2π / 3 )), digits = 2),
+    ]
+    for n in directions
+        c = round(n + k, digits = 2)
+        c ∈ keys(grid) && return true
+    end
+    return false
 end
 
 function g(data)
     tiles = split(data, '\n')
     tiles = find_steps.(tiles)
 
-    grid = DefaultDict{Complex, Bool}(false)
+    grid = Dict{Complex, Bool}()
     for tile in tiles
         flip!(grid, tile)
     end
-    # @show grid
-    minx, maxx = extrema([x.re for x in keys(grid)])
-    miny, maxy = extrema([y.im for y in keys(grid)])
+
+    directions = [
+        round(exp(im * 0), digits = 2),
+        round(exp(im * π / 3), digits = 2),
+        round(exp(im * 2π / 3), digits = 2),
+        round(exp(im * π), digits = 2),
+        round(exp(im * ( π + π / 3 )), digits = 2),
+        round(exp(im * ( π + 2π / 3 )), digits = 2),
+    ]
+
+    [delete!(grid, k) for k in keys(grid) if !grid[k]]
+
+    day = 1
+    while day <= 100
+        for k in keys(grid), n in directions
+            c = round(k + n, digits = 2)
+            if c ∉ keys(grid) && has_neighbour(grid, c)
+                grid[c] = false
+            end
+        end
+
+        flipped = Complex[]
+        for k in keys(grid)
+            counter = count([grid[round(k + n, digits = 2)] for n in directions if round(k + n, digits = 2) ∈ keys(grid)])
+            if grid[k]
+                ( counter == 0 || counter > 2 ) && push!(flipped, k)
+            else
+                counter == 2 && push!(flipped, k)
+            end
+        end
+        for k in flipped
+            grid[k] = !grid[k]
+        end
+        [delete!(grid, k) for k in keys(grid) if !grid[k]]
+        println("Day $day: $(count(values(grid)))")
+        day += 1
+    end
+    count(values(grid))
 end
