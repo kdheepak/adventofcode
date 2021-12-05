c21d8d20e43dde45ae28318ef1cf45c543154516
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-05 10:36:16 -0700

    Update bench

--- 2021/benches/benchmark.rs
+++ 2021/benches/benchmark.rs
@@ -1,15 +1,15 @@
 use aoc2021::*;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-  for day in &[1, 2, 3, 4, 5] {
+  for day in [1, 2, 3, 4, 5].iter().rev() {
     let input = get_input(*day);
     let problem = get_problem(*day).expect("Unable to create problem.");
     c.bench_function(format!("Day {:02} part 1", day).as_str(), |b| {
-      b.iter(|| problem.part1(black_box(&input)).unwrap())
+      b.iter(|| problem.part1(&input).unwrap())
     });
     c.bench_function(format!("Day {:02} part 2", day).as_str(), |b| {
-      b.iter(|| problem.part2(black_box(&input)).unwrap())
+      b.iter(|| problem.part2(&input).unwrap())
     });
   }
 }
