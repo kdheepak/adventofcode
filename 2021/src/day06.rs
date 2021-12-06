95c2e9696634b34688b3b46dee7ed39352edbed7
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-06 01:01:31 -0700

    Use array rotate_left for day6

--- 2021/src/day06.rs
+++ 2021/src/day06.rs
@@ -1,6 +1,3 @@
-use ansi_term::Style;
-use itertools::Itertools;
-
 use crate::problem::Problem;
 
 #[derive(Default)]
@@ -9,51 +6,25 @@ pub struct Day06 {}
 impl Day06 {
 }
 
+fn helper(input: &str, days: usize) -> usize {
+    let fish: Vec<usize> = input.split(',').map(|x| x.parse().unwrap()).collect();
+    let mut counter = vec![0; 9];
+    // initialization
+    for f in fish { counter[f] += 1 }
+    for _ in 0..days {
+      counter.rotate_left(1);
+      counter[6]+=counter[8];
+    }
+    counter.iter().sum()
+}
+
 impl Problem for Day06 {
   fn part1(&self, input: &str) -> Option<String> {
-    let mut fish: Vec<usize> = input.split(',').map(|x| x.parse().unwrap()).collect();
-
-    let mut day = 0;
-    while day < 80 {
-      let mut update = vec![];
-      for f in fish.iter_mut() {
-        if *f == 0 {
-          *f = 6;
-          update.push(8);
-        } else {
-          *f -= 1;
-        }
-      }
-      for f in update {
-        fish.push(f);
-      }
-      day += 1;
-    }
-    Some(fish.len().to_string())
+    Some(helper(input, 80).to_string())
   }
 
   fn part2(&self, input: &str) -> Option<String> {
-    let fish: Vec<usize> = input.split(',').map(|x| x.parse().unwrap()).collect();
-    let mut counter = vec![0; 9];
-
-    // initialization
-    for f in fish {
-      counter[f] += 1;
-    }
-
-    for _ in 0..256 {
-      let f = counter[0];
-      for i in 0..counter.len() {
-        if i == (counter.len() - 1) {
-          counter[i] = f;
-        } else {
-          counter[i] = counter[i + 1]
-        }
-      }
-      counter[6] += f;
-    }
-
-    Some(counter.iter().sum::<usize>().to_string())
+    Some(helper(input, 256).to_string())
   }
 }
 
