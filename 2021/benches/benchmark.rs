0552dc9d7f3a6278c4b3335b680fb02eb8296dcc
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-06 22:50:37 -0700

    Add bench

--- 2021/benches/benchmark.rs
+++ 2021/benches/benchmark.rs
@@ -2,7 +2,7 @@ use aoc2021::*;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-  for day in [1, 2, 3, 4, 5, 6].iter().rev() {
+  for day in [1, 2, 3, 4, 5, 6, 7].iter().rev() {
     let input = get_input(*day);
     let problem = get_problem(*day).expect("Unable to create problem.");
     c.bench_function(format!("Day {:02} part 1", day).as_str(), |b| {
