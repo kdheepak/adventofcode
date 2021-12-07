f753fd730887d90d7bc2c0c165daa45726ba0921
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-06 23:29:54 -0700

    Update day7

--- 2021/src/day07.rs
+++ 2021/src/day07.rs
@@ -10,7 +10,7 @@ impl Day07 {
 impl Problem for Day07 {
   fn part1(&self, input: &str) -> Option<String> {
     let positions: Vec<i32> = input.split(',').map(|p| p.parse().unwrap()).collect();
-    let (min, max) = positions.iter().minmax().into_option().unwrap();
+    let (min, max) = positions.iter().minmax().into_option()?;
     (*min..=*max)
       .map(|pos| positions.iter().fold(0, |acc, p| acc + (p - pos).abs()))
       .min()
@@ -19,7 +19,7 @@ impl Problem for Day07 {
 
   fn part2(&self, input: &str) -> Option<String> {
     let positions: Vec<i32> = input.split(',').map(|p| p.parse().unwrap()).collect();
-    let (min, max) = positions.iter().minmax().into_option().unwrap();
+    let (min, max) = positions.iter().minmax().into_option()?;
     (*min..=*max)
       .map(|pos| positions.iter().fold(0, |acc, p| acc + (p - pos).abs() * ((p - pos).abs() + 1) / 2))
       .min()
