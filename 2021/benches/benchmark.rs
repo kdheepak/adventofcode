808154149daa056fbff1202a9942daf4fb4ddf07
Dheepak Krishnamurthy <me@kdheepak.com>
2021-12-08 21:55:44 -0700

    chore: Setup for day 9

--- 2021/benches/benchmark.rs
+++ 2021/benches/benchmark.rs
@@ -5,12 +5,8 @@ fn criterion_benchmark(c: &mut Criterion) {
   for day in [1, 2, 3, 4, 5, 6, 7, 8, 9].iter().rev() {
     let input = get_input(*day);
     let problem = get_problem(*day).expect("Unable to create problem.");
-    c.bench_function(format!("Day {:02} part 1", day).as_str(), |b| {
-      b.iter(|| problem.part1(&input).unwrap())
-    });
-    c.bench_function(format!("Day {:02} part 2", day).as_str(), |b| {
-      b.iter(|| problem.part2(&input).unwrap())
-    });
+    c.bench_function(format!("Day {:02} part 1", day).as_str(), |b| b.iter(|| problem.part1(&input).unwrap()));
+    c.bench_function(format!("Day {:02} part 2", day).as_str(), |b| b.iter(|| problem.part2(&input).unwrap()));
   }
 }
 
